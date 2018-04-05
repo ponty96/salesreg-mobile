d508f649543f2230b4c661d87616935186296cea
Klaz <giwaklaz@gmail.com>
2018-04-05 10:45:33 +0100

    Renamed InputForText to InputAtom.

---
+++ src/Atom/InputAtom.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { TextInput, View, StyleSheet, Text } from 'react-native';
+
+export default class InputForText extends Component {
+  render() {
+    return (
+      <View style = {styles.wrapper}>
+        <View style = { styles.wrapperForAsterikAndTextInput }>
+          <Text style = {styles.asterik}>
+            {(this.props.required === true) ? '*' : ''}
+          </Text>
+          <TextInput 
+          placeholder = { this.props.placeholder }
+          style = { styles.input }
+          keyboardType = { this.props.keyboardType }
+          underlineColorAndroid = 'transparent'
+          />
+        </View>
+        <Text style = {styles.bottomLabel}>
+          { this.props.bottomLabel }
+        </Text>
+      </View>
+    );
+
+    
+  }
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    marginHorizontal: 10
+  },
+  wrapperForAsterikAndTextInput: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey',
+  },
+  asterik: {
+    color: 'red',
+    marginTop: 13,
+    fontSize: 20
+  },
+  input: {
+    color: 'grey',
+    marginRight: 10,
+    marginBottom: 7,
+    marginTop: 15,
+    fontSize: 17,
+    flex: 1,
+    marginLeft: 1,
+  },
+  bottomLabel: {
+    color: 'lightgrey',
+  }
+});
