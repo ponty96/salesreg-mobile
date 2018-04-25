c2749190746b28f2f8e4bc856d3532eb961aa1ae
Jerry Heir <nwaezejerry@gmail.com>
2018-04-25 14:52:50 +0100

    OldOrderList Updated, MainOrderList has been added ... Moving to debts Screen

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 
 import Routes from './src/Navigation/Routes';
-<<<<<<< HEAD
 //import MainOrderList from './src/Components/MainOrderList';
-=======
-//import CustomerDetailScreen from './src/Screen/CustomerDetailScreen';
->>>>>>> c31475f1193f8893654515a7923aafc796b93364
 
 export default class App extends React.Component {
   render() {
     return (
 	  <Routes />
-	  //<CustomerDetailScreen />
     );
   }
 }
