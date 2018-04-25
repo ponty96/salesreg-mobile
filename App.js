625716d757749e058a1950f8a5b32ef3216b6339
Klaz <giwaklaz@gmail.com>
2018-04-25 22:50:09 +0100

    Conflict solved

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 
-<<<<<<< HEAD
 //import Routes from './src/Navigation/Routes';
 import CustomerDetailScreen from './src/Screen/CustomerDetailScreen';
-=======
-import Routes from './src/Navigation/Routes';
-//import MainOrderList from './src/Components/MainOrderList';
->>>>>>> c2749190746b28f2f8e4bc856d3532eb961aa1ae
 
 export default class App extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
 	  //<Routes />
 	  <CustomerDetailScreen />
-=======
-	  <Routes />
->>>>>>> c2749190746b28f2f8e4bc856d3532eb961aa1ae
     );
   }
 }
