44c1e9600051141d20db80e9c09bc05083d52fe3
Jerry Heir <nwaezejerry@gmail.com>
2018-06-06 10:46:21 +0100

    Review Changes By Jeremiah Heir

--- src/Atom/AccordionAtom.js
+++ src/Atom/AccordionAtom.js
@@ -1,13 +1,13 @@
-import * as React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { Icon } from 'native-base'
-import Accordion from 'react-native-collapsible/Accordion'
-import DebtAccordionAtom from './DebtAccordionAtom'
-import { ScrollView } from 'react-native-gesture-handler'
-import { sections } from '../config/data'
-import { color } from '../Style/Color'
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Icon } from 'native-base';
+import Accordion from 'react-native-collapsible/Accordion';
+import DebtAccordionAtom from './DebtAccordionAtom';
+import { ScrollView } from 'react-native-gesture-handler';
+import { sections } from '../config/data';
+import { color } from '../Style/Color';
 
-const SECTIONS = sections
+const SECTIONS = sections;
 
 /*interface IProps {
   icon: string
@@ -16,29 +16,29 @@ interface IState {
   activeSection: boolean
   icon: string
 }*/
-const namer = 'md-arrow-dropdown'
+const namer = 'md-arrow-dropdown';
 
 export default class AccordionAtom extends React.Component /*<IProps, IState>*/ {
   constructor(props /*?: IProps, context?: any*/) {
-    super(props /*, context*/)
+    super(props /*, context*/);
     this.state = {
-      activeSection: false,
-      icon: 'md-arrow-dropdown'
-    }
+      activeSection: false
+      // icon: 'md-arrow-dropdown'
+    };
   }
-  _setHeader(section /*: any*/) {
-    const { icon } = this.state
-    this._loadHeader(icon)
-    this.setState({ activeSection: section })
-  }
-  _loadHeader = (a /*: string*/) => {
-    if (a === 'md-arrow-dropdown') {
-      this.setState({ icon: 'md-arrow-dropup' })
-    } else {
-      this.setState({ icon: 'md-arrow-dropdown' })
-    }
-  }
-  _renderHeader(section /*: any*/) {
+  setHeader = (section /*: any*/) => {
+    //const { icon } = this.state
+    // this.loadHeader(icon)
+    this.setState({ activeSection: section });
+  };
+  // loadHeader = (a /*: string*/) => {
+  // if (a === 'md-arrow-dropdown') {
+  //  this.setState({ icon: 'md-arrow-dropup' })
+  // } else {
+  //  this.setState({ icon: 'md-arrow-dropdown' })
+  //}
+  // }
+  renderHeader(section /*: any*/) {
     return (
       <View style={styles.mainAccord}>
         <View style={styles.accordView1}>
@@ -60,26 +60,26 @@ export default class AccordionAtom extends React.Component /*<IProps, IState>*/ 
           <Icon style={styles.accordIcon} name={namer} />
         </View>
       </View>
-    )
+    );
   }
 
-  _renderContent() {
-    return <DebtAccordionAtom />
+  renderContent() {
+    return <DebtAccordionAtom />;
   }
 
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <ScrollView>
         <Accordion
           sections={SECTIONS}
           activeSection={this.state.activeSection}
-          renderHeader={this._renderHeader}
-          renderContent={this._renderContent}
-          onChange={this._setHeader.bind(this)}
+          renderHeader={this.renderHeader}
+          renderContent={this.renderContent}
+          onChange={this.setHeader}
         />
       </ScrollView>
-    )
+    );
   }
 }
 
@@ -134,4 +134,4 @@ const styles = StyleSheet.create({
   accordTextL2: {
     color: color.primary
   }
-})
+});
