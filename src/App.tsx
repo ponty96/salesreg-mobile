439d43acb4783f4ad1372dbb965351a579f2d624
Tobi Taiwo <ttobi4@gmail.com>
2018-06-13 15:56:52 +0100

    rebase to master

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from 'react-apollo';
 
 import Routes from './Navigation/Routes'
 
-const client = new ApolloClient({uri: 'https://github.com'});
-
 export default class App extends React.Component {
   render() {
-      return <ApolloProvider client={client}>
-          <Routes />
-      </ApolloProvider>
+    return <Routes />
   }
 }
