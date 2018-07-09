dc2336c389c45cd236552d8bfd59b47cb107259b
klaz <giwaklaz@gmail.com>
2018-07-09 11:54:07 +0100

    JIRA-ISSUE-SM-25: Added CustomHeader to CustomerScreen.

--- src/Screen/CustomerScreen.tsx
+++ src/Screen/CustomerScreen.tsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import { View, StyleSheet } from 'react-native'
-import { Icon } from 'native-base'
 
 import FabAtom from './../Atom/FabAtom'
 import CustomerList from '../Components/CustomerList'
 import { color } from '../Style/Color'
 import { customerList } from '../config/data'
+import CustomHeader from '../Components/CustomHeader'
 
 interface IProps {
   navigation: any
@@ -15,21 +15,18 @@ interface IState {}
 
 class CustomerScreen extends Component<IProps, IState> {
   static navigationOptions = ({ navigation }: any) => {
-    const { params } = navigation.state
-    let right = <Icon name={'ios-search'} style={styles.headerIcon} />
-    let left = params &&
-      params.items &&
-      params.items.length > 0 && (
-        <Icon
-          name={'menu'}
-          onPress={() => navigation.navigate('DrawerToggle')}
-          style={styles.headerIcon}
-        />
-      )
     return {
       title: 'Kay5iveAttractions',
-      headerRight: right,
-      headerLeft: left
+      header: (
+        <CustomHeader
+          title="Customer"
+          menu
+          onMenuPress={() => navigation.navigate('DrawerToggle')}
+          right
+          firstRightIcon="ios-search"
+          rightText=" "
+        />
+      )
     }
   }
   onPress = () => {
