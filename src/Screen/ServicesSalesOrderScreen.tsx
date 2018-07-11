87bbf9eda5bb16a2596b6a6db2bf8ad8f2b976cb
klaz <giwaklaz@gmail.com>
2018-07-11 14:57:49 +0100

    JIRA-ISSUE-SM-25: Fixed conflicts.

--- src/Screen/ServicesSalesOrderScreen.tsx
+++ src/Screen/ServicesSalesOrderScreen.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
-export default class ServiceSalesOrderScreen extends Component {
+export default class ServicesSalesOrderScreen extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
