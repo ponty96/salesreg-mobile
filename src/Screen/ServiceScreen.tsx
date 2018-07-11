3e2a3e32a1438e91df17567eabbb7504e4df9a64
klaz <giwaklaz@gmail.com>
2018-07-11 17:25:04 +0100

    JIRA-ISSUE-SM-27: Added custome header to ServiceScreen.

--- src/Screen/ServiceScreen.tsx
+++ src/Screen/ServiceScreen.tsx
@@ -30,6 +30,7 @@ export default class ServiceScreen extends Component<IProps> {
             })
           }
           onBackPress={() => navigation.goBack()}
+          showRight
         />
       )
     }
