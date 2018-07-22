e8b83fd61bc571e9b79910935d56870892a41893
Ayomide Aregbede <ayo.aregbede@gmail.com>
2018-07-22 14:32:11 +0100

    remove unnecessary component

--- src/Components/ProductDetails.tsx
+++ src/Components/ProductDetails.tsx
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react';
-import { View, StyleSheet } from 'react-native';
-import ProductDetailAtom from '../Atom/ProductDetailAtom';
+import { View, StyleSheet, Text } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Thumbnail, ListItem, Left, Right } from 'native-base';
+import { color } from '../Style/Color';
 
 interface IProps {
   navigation: any;
@@ -37,14 +38,76 @@ class ProductDetails extends PureComponent<IProps, IState> {
     return (
       <View style={styles.ababa}>
         <ScrollView>
-          <ProductDetailAtom
-            product={product.name}
-            packs={300}
-            currentStockQuantity={product.number}
-            unitCostPrice={parseFloat(product.costPrice)}
-            sellingPrice={parseFloat(product.sellingPrice)}
-            minStockQuantity={product.minimumStockQuantity}
-          />
+          <View>
+            <View style={styles.aboveAccordionContainerP}>
+              <View style={styles.aboveAccordionPictureViewP}>
+                <Thumbnail
+                  source={{
+                    uri:
+                      'https://snack-code-uploads.s3.us-west-1.amazonaws.com/~asset/9d799c33cbf767ffc1a72e53997218f7'
+                  }}
+                  style={styles.aboveAccordiondpP}
+                />
+                <Text style={styles.aboveAccordionPictureText}>
+                  {product.name}
+                </Text>
+              </View>
+              <View style={styles.aboveAccordionMoneyView}>
+                <View style={styles.viewMarginRight}>
+                  <Text style={styles.aboveAccordionGreyFont}>
+                    Stock quantity(in units)
+                  </Text>
+                  <Text style={styles.aboveAccordionBoldFont}>
+                    {product.number}
+                  </Text>
+                </View>
+                <View style={styles.viewMarginRight}>
+                  <Text style={styles.aboveAccordionGreyFont}>
+                    Total Units Sold
+                  </Text>
+                  <Text style={styles.aboveAccordionBoldFont}>{300}</Text>
+                </View>
+              </View>
+            </View>
+            <View>
+              <ListItem style={styles.aboveAccordionWhiteList}>
+                <Left>
+                  <Text style={styles.aboveAccordionBlackTextL}>
+                    Unit cost price
+                  </Text>
+                </Left>
+                <Right>
+                  <Text style={styles.aboveAccordionBlackTextR}>
+                    &#8358; {parseFloat(product.costPrice)}
+                  </Text>
+                </Right>
+              </ListItem>
+              <ListItem style={styles.aboveAccordionWhiteList}>
+                <Left>
+                  <Text style={styles.aboveAccordionBlackTextL}>
+                    Selling Price
+                  </Text>
+                </Left>
+                <Right>
+                  <Text style={styles.aboveAccordionGreenTextR}>
+                    &#8358; {parseFloat(product.sellingPrice)}
+                  </Text>
+                </Right>
+              </ListItem>
+              <ListItem style={styles.aboveAccordionWhiteList}>
+                <Left>
+                  <Text style={styles.aboveAccordionBlackTextL}>
+                    Minimum stock quantity
+                  </Text>
+                </Left>
+                <Right>
+                  <Text style={styles.aboveAccordionBlackTextR}>
+                    {product.minimumStockQuantity}
+                  </Text>
+                </Right>
+              </ListItem>
+            </View>
+          </View>
         </ScrollView>
       </View>
     );
@@ -68,5 +131,92 @@ const styles = StyleSheet.create({
   txtP: {
     color: '#fff',
     fontSize: 16
+  },
+  aboveAccordionContainerP: {
+    flexDirection: 'row',
+    flex: 0,
+    borderTopWidth: 0.5,
+    borderBottomWidth: 0.5,
+    borderTopColor: '#f0f0f0',
+    borderBottomColor: '#c0c0c0'
+  },
+  aboveAccordionPictureViewP: {
+    flexDirection: 'column',
+    width: '50%',
+    height: 170,
+    alignItems: 'flex-start',
+    padding: 16,
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  aboveAccordiondpP: {
+    height: 60,
+    width: 60
+  },
+  aboveAccordionBoldFont: {
+    fontFamily: 'SourceSansPro_Semibold',
+    fontSize: 20,
+    textAlign: 'right'
+  },
+  aboveAccordionWhiteList: {
+    height: 65,
+    width: '100%',
+    backgroundColor: '#FFF',
+    paddingLeft: 0,
+    marginLeft: 0
+  },
+  aboveAccordionBlackTextL: {
+    fontSize: 16,
+    color: color.dropdown,
+    paddingLeft: 16,
+    fontFamily: 'SourceSansPro'
+  },
+  aboveAccordionBlackTextR: {
+    fontSize: 16,
+    color: '#000',
+    fontFamily: 'SourceSansPro'
+  },
+  aboveAccordionGreenTextR: {
+    fontSize: 16,
+    color: color.selling,
+    fontFamily: 'SourceSansPro'
+  },
+  aboveAccordionRedTextR: {
+    fontSize: 16,
+    color: 'red',
+    fontFamily: 'SourceSansPro'
+  },
+  aboveAccordionGreyText: {
+    fontSize: 16,
+    color: color.dropdown,
+    paddingLeft: 16,
+    fontFamily: 'SourceSansPro'
+  },
+  aboveAccordionGreyFont: {
+    fontSize: 17,
+    color: '#000',
+    fontFamily: 'SourceSansPro'
+  },
+  aboveAccordionRedText: {
+    fontSize: 16,
+    color: 'red',
+    fontFamily: 'SourceSansPro'
+  },
+  aboveAccordionPictureText: {
+    paddingTop: 10,
+    fontSize: 18,
+    fontWeight: '400',
+    color: color.menu,
+    fontFamily: 'SourceSansPro'
+  },
+  aboveAccordionMoneyView: {
+    width: '50%',
+    height: 170,
+    alignItems: 'flex-end',
+    justifyContent: 'center'
+    // backgroundColor: '#FFF',
+  },
+  viewMarginRight: {
+    marginRight: 16
   }
 });
