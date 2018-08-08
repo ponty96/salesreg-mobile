71ec91df83fafa9b9de6651294453a8d44b25796
klaz <giwaklaz@gmail.com>
2018-08-08 18:41:32 +0100

    Completed the expenses list screen.

--- src/Atom/SalesOrderListAtom.tsx
+++ src/Atom/SalesOrderListAtom.tsx
@@ -5,12 +5,13 @@ import { Left, Right } from 'native-base'
 
 interface IProps {
   firstTopLeftText: string
-  bottomLeftText: string
+  bottomLeftText?: string
   secondTopLeftText?: string
-  topRightText: number
-  bottomRightText: string
+  topRightText: string
+  bottomRightText?: string
   onPress?: () => void
   style?: object
+  rightTopTextStyle?: object
 }
 
 const renderStatusIndicator = (bottomRightText: string): JSX.Element => {
@@ -41,7 +42,7 @@ const renderStatusIndicator = (bottomRightText: string): JSX.Element => {
   return <View style={[styles.statusIndicator, { backgroundColor: colour }]} />
 }
 
-const salesOrderListAtom = (props: IProps) => {
+const SalesOrderListAtom = (props: IProps) => {
   return (
     <TouchableOpacity
       style={[styles.wrapper, props.style]}
@@ -59,7 +60,9 @@ const salesOrderListAtom = (props: IProps) => {
         <Text style={[styles.text, styles.bottom]}>{props.bottomLeftText}</Text>
       </Left>
       <Right style={styles.rightWrapper}>
-        <Text style={[styles.text, styles.top]}>{props.topRightText}</Text>
+        <Text style={[styles.text, styles.top, props.rightTopTextStyle]}>
+          {props.topRightText}
+        </Text>
         <Text style={[styles.status, styles.bottom]}>
           {props.bottomRightText}
         </Text>
@@ -69,7 +72,7 @@ const salesOrderListAtom = (props: IProps) => {
   )
 }
 
-export default salesOrderListAtom
+export default SalesOrderListAtom
 
 const styles = StyleSheet.create({
   wrapper: {
--- src/Screen/ExpensesScreen.tsx
+++ src/Screen/ExpensesScreen.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
-import { View, StyleSheet, Alert, SectionList } from 'react-native'
+import { View, StyleSheet, Alert, SectionList, Text } from 'react-native'
 import CustomHeader from '../Components/CustomHeader'
 import FabAtom from '../Atom/FabAtom'
 import EmptyList from '../Components/EmptyList'
+import SalesOrderListAtom from '../Atom/SalesOrderListAtom'
+import { color } from '../Style/Color'
 
 interface IProps {
   navigation: any
@@ -17,13 +19,80 @@ export default class ExpensesScreen extends React.Component<IProps> {
           showMenu
           onPressFirstRightIcon={() => Alert.alert('Search button pressed.')}
           onMenuPress={() => navigation.navigate('DrawerToggle')}
+          showRight
+          firstRightIcon="ios-search"
         />
       )
     }
   }
 
+  renderList = ({ item }: any): JSX.Element => {
+    return (
+      <SalesOrderListAtom
+        firstTopLeftText={item.name}
+        bottomLeftText={item.paidTo}
+        topRightText={'\u20A6 ' + item.amount}
+        rightTopTextStyle={styles.amount}
+      />
+    )
+  }
+
+  renderListFooter = ({ section }: any): JSX.Element => {
+    return (
+      <View style={styles.footerWrapper}>
+        <Text style={styles.footerText}>{section.date}</Text>
+      </View>
+    )
+  }
+
   render() {
     const { navigation } = this.props
+    const DATA0: Array<{ name: string; paidTo: string; amount: string }> = [
+      {
+        name: 'Shop renovation',
+        paidTo: 'Lakan Wanton Doe',
+        amount: '3000.00'
+      },
+      {
+        name: 'Fuel',
+        paidTo: 'Unknown',
+        amount: '11,350.00'
+      },
+      {
+        name: 'Order PU0017',
+        paidTo: 'Rakuxi shoes',
+        amount: '20,350.00'
+      },
+      {
+        name: 'Fuel',
+        paidTo: 'Unknown',
+        amount: '11,350.00'
+      }
+    ]
+
+    const DATA1: Array<{ name: string; paidTo: string; amount: string }> = [
+      {
+        name: 'Order PU0012',
+        paidTo: 'Lere Wakoza',
+        amount: '200,000.00'
+      },
+      {
+        name: 'Order PU0013',
+        paidTo: 'Lere Wakoza',
+        amount: '200,000.00'
+      },
+      {
+        name: 'Nepa bill',
+        paidTo: 'PHCN',
+        amount: '5780.00'
+      },
+      {
+        name: 'Shelve',
+        paidTo: 'Carpenter',
+        amount: '12,800.00'
+      }
+    ]
+
     return (
       <View style={styles.container}>
         <FabAtom
@@ -33,10 +102,16 @@ export default class ExpensesScreen extends React.Component<IProps> {
           type="MaterialCommunityIcons"
         />
         <SectionList
-          sections={[]}
+          renderItem={this.renderList}
           ListEmptyComponent={
             <EmptyList type={{ Text: 'expenses', verifyMainList: 'main' }} />
           }
+          sections={[
+            { date: 'YESTERDAY', data: DATA0 },
+            { date: '23 APRIL 2018', data: DATA1 }
+          ]}
+          keyExtractor={item => item.name}
+          renderSectionFooter={this.renderListFooter}
         />
       </View>
     )
@@ -46,7 +121,19 @@ export default class ExpensesScreen extends React.Component<IProps> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: color.secondary
+  },
+  footerText: {
+    fontFamily: 'SourceSansPro_Semibold',
+    fontSize: 16,
+    textAlign: 'center'
+  },
+  footerWrapper: {
+    backgroundColor: color.grey,
+    marginHorizontal: 10,
+    paddingVertical: 8
+  },
+  amount: {
+    color: color.selling
   }
 })
