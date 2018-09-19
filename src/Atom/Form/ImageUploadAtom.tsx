c1e7466fa374c6533ff9e6b2ab28d4d1825be049
Ayomide Aregbede <ayo.aregbede@gmail.com>
2018-09-19 15:06:16 +0100

    add image-crop-picker library to ImageUploadAtom

--- src/Atom/Form/ImageUploadAtom.tsx
+++ src/Atom/Form/ImageUploadAtom.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { View, StyleSheet, Image, Text, TouchableHighlight } from 'react-native'
 import Icon from '../Icon'
 import { color } from '../../Style/Color'
+import ImagePicker from 'react-native-image-crop-picker'
+import { ActionSheet } from 'native-base'
 
 interface IProps {
   images: string[]
@@ -10,13 +12,61 @@ interface IProps {
   error?: any
 }
 export default class ImageUploadAtom extends React.PureComponent<IProps> {
+  handleImageUpload = () => {
+    ActionSheet.show(
+      {
+        options: ['Take Photo', 'Photo Library', 'Cancel'],
+        cancelButtonIndex: 2,
+        title: 'Upload Image'
+      },
+      buttonIndex => {
+        switch (buttonIndex) {
+          case 0:
+            this.openCamera()
+          case 1:
+            this.openGallery()
+          default:
+            break
+        }
+      }
+    )
+  }
+
+  openGallery = () => {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 300,
+      cropping: true,
+      multiple: true,
+      mediaType: 'photo',
+      includeBase64: true
+    }).then(images => {
+      console.log(images)
+    })
+  }
+
+  openCamera = () => {
+    ImagePicker.openCamera({
+      width: 300,
+      height: 300,
+      cropping: true,
+      multiple: true,
+      mediaType: 'photo',
+      includeBase64: true
+    }).then(images => {
+      console.log('images', images)
+    })
+  }
   render() {
     return (
       <View>
-        <TouchableHighlight style={styles.placeholderWrapper}>
+        <TouchableHighlight
+          style={styles.placeholderWrapper}
+          onPress={this.handleImageUpload}
+        >
           {this.renderMainImagePlaceholder()}
         </TouchableHighlight>
-        <TouchableHighlight>
+        <TouchableHighlight onPress={this.handleImageUpload}>
           <Text
             style={{
               color: color.button,
