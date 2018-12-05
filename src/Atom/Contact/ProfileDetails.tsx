f2e4f1f612e0f907084dd1212aa3b18b731ac4a0
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2018-12-05 16:33:29 +0100

    Change all images in the app to use the CachedImageAtom component

--- src/Atom/Contact/ProfileDetails.tsx
+++ src/Atom/Contact/ProfileDetails.tsx
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react'
 import { color } from '../../Style/Color'
 import ProfileListAtom from '../../Atom/ListItem/ExpandableListItemAtom'
-import { StyleSheet, View, FlatList, Image, Text } from 'react-native'
+import { StyleSheet, View, FlatList, Text } from 'react-native'
+import CachedImageAtom from '../CachedImageAtom'
 
 interface Section {
   section: string
@@ -35,12 +36,11 @@ export default class ProfileDetails extends PureComponent<IProps> {
     if (index == 0) {
       return (
         <View style={styles.pictureView}>
-          <Image
-            source={{
-              uri:
-                this.props.image ||
-                'https://snack-code-uploads.s3.us-west-1.amazonaws.com/~asset/9d799c33cbf767ffc1a72e53997218f7'
-            }}
+          <CachedImageAtom
+            uri={
+              this.props.image ||
+              'https://snack-code-uploads.s3.us-west-1.amazonaws.com/~asset/9d799c33cbf767ffc1a72e53997218f7'
+            }
             style={{ width: '100%', height: 280 }}
           />
         </View>
