25a086d966cc47d55e7c982b09055940b193372c
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2018-12-14 20:05:52 +0100

    Hook the restock branch with graphql

--- src/Screen/Store/UpsertProductRestockScreen.tsx
+++ src/Screen/Store/UpsertProductRestockScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import FormStepperContainer from '../../Container/Form/StepperContainer'
-// import { Mutation } from 'react-apollo'
+import { Mutation } from 'react-apollo'
 import AppSpinner from '../../Components/Spinner'
-// import { parseFieldErrors } from '../../Functions'
+import { parseFieldErrors } from '../../Functions'
 import { UserContext } from '../../context/UserContext'
+import { RestockProducts } from '../../graphql/mutations/store'
+import { ListCompanyProductsGQL } from '../../graphql/queries/store'
 
 interface IProps {
   navigation: any
@@ -45,57 +47,105 @@ class UpsertProductRestockScreen extends React.PureComponent<IProps, IState> {
       [key]: val
     }
 
-    console.log("The value is ", val)
-
     this.setState({
       ...formData
     })
   }
 
-  onCompleted = async () => {}
+  onCompleted = async res => {
+    const {
+      restockProducts: { success, fieldErrors }
+    } = res
 
-  parseMutationVariables = () => {}
+    if (!success) {
+      this.setState({ fieldErrors: parseFieldErrors(fieldErrors) })
+    } else {
+      this.props.navigation.navigate('Products')
+    }
+  }
+
+  parseMutationVariables = () => {
+    let { items } = this.state,
+      params = items.map(item => {
+        let newItems = { ...item }
+
+        delete newItems.name
+        delete newItems.type
+        delete newItems.serviceId
+        delete newItems.unitPrice
+
+        return newItems
+      })
+
+    return {
+      items: params
+    }
+  }
 
   render() {
     return (
-      <React.Fragment>
-        <AppSpinner visible={false} />
-        <FormStepperContainer
-          formData={this.state}
-          updateValueChange={this.updateState}
-          handleBackPress={() => this.props.navigation.goBack()}
-          fieldErrors={this.state.fieldErrors}
-          onCompleteSteps={() => null}
-          steps={[
-            {
-              stepTitle: "Let's have the items you are restocking",
-              formFields: [
-                {
-                  label: '',
-                  validators: ['sales-order'],
-                  type: {
-                    type: 'restock-items'
-                  },
-                  name: 'items'
-                }
-              ]
-            },
-            {
-              stepTitle: 'Verify your re-stock entry',
-              formFields: [
-                {
-                  label: '',
-                  type: {
-                    type: 'product-list'
-                  },
-                  name: 'items'
-                }
-              ],
-              buttonTitle: 'Done'
+      <Mutation
+        mutation={RestockProducts}
+        refetchQueries={[
+          {
+            query: ListCompanyProductsGQL,
+            variables: {
+              companyId: this.props.user.company.id,
+              first: 10,
+              after: null
             }
-          ]}
-        />
-      </React.Fragment>
+          }
+        ]}
+        awaitRefetchQueries={true}
+        onCompleted={this.onCompleted}
+      >
+        {(restockProducts, { loading }) => {
+          return (
+            <React.Fragment>
+              <AppSpinner visible={loading} />
+              <FormStepperContainer
+                formData={this.state}
+                updateValueChange={this.updateState}
+                handleBackPress={() => this.props.navigation.goBack()}
+                fieldErrors={this.state.fieldErrors}
+                onCompleteSteps={() =>
+                  restockProducts({
+                    variables: this.parseMutationVariables()
+                  })
+                }
+                steps={[
+                  {
+                    stepTitle: "Let's have the items you are restocking",
+                    formFields: [
+                      {
+                        label: '',
+                        validators: ['sales-order'],
+                        type: {
+                          type: 'restock-items'
+                        },
+                        name: 'items'
+                      }
+                    ]
+                  },
+                  {
+                    stepTitle: 'Verify your re-stock entry',
+                    formFields: [
+                      {
+                        label: '',
+                        type: {
+                          type: 'product-list'
+                        },
+                        name: 'items'
+                      }
+                    ],
+                    buttonTitle: 'Done'
+                  }
+                ]}
+              />
+            </React.Fragment>
+          )
+        }}
+      </Mutation>
     )
   }
 }
