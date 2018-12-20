4e207ce668b2106f49b415e30b2a172786794c9e
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2018-12-20 13:31:45 +0100

    Add Splashscreen to the app

--- android/app/src/main/java/com/salesreg/MainApplication.java
+++ android/app/src/main/java/com/salesreg/MainApplication.java
@@ -3,6 +3,7 @@ package com.salesreg;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import me.hauvo.thumbnail.RNThumbnailPackage;
 import com.arttitude360.reactnative.rnpaystack.RNPaystackPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new RNThumbnailPackage(),
             new RNPaystackPackage(),
             new PickerPackage(),
