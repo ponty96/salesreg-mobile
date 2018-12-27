34d7797820f7fb5a1f9df7237fa1a2677036d50c
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2018-12-27 21:41:37 +0100

    Jira issue yip 99 (#129)
    * Add content to homescreen
    
    * Add content to homescreen

--- src/Screen/HomeScreen.tsx
+++ src/Screen/HomeScreen.tsx
@@ -3,6 +3,7 @@ import { View, StyleSheet, Text, Dimensions } from 'react-native'
 import { color } from '../Style/Color'
 import Header from '../Components/Header/BaseHeader'
 import Auth from '../services/auth'
+import { Content } from 'native-base'
 
 interface IProps {
   navigation: any
@@ -43,9 +44,48 @@ export default class HomeScreen extends React.Component<IProps, IState> {
     return (
       <React.Fragment>
         <View style={styles.container}>
-          <View style={styles.homeBackground}>
-            <Text style={styles.homeText}>Welcome {this.state.username}!</Text>
-          </View>
+          <Content>
+            <View style={styles.homeBackground}>
+              <Text style={styles.homeText}>
+                Welcome {this.state.username}!
+              </Text>
+            </View>
+            <Text style={[styles.text, { marginVertical: 20 }]}>
+              Tap he menu icon on the top right hand corner of the screen to
+              find all the menu options you will be needing:
+            </Text>
+            <View style={styles.section}>
+              <View style={styles.dot} />
+              <Text style={[styles.text, { marginTop: -4 }]}>
+                Scroll down to find the settings menu where you can edit your
+                profile
+              </Text>
+            </View>
+            <View style={styles.section}>
+              <View style={styles.dot} />
+              <Text style={[styles.text, { marginTop: -4 }]}>
+                Use the products and services menu to manage your inventory
+              </Text>
+            </View>
+            <View style={styles.section}>
+              <View style={styles.dot} />
+              <Text style={[styles.text, { marginTop: -4 }]}>
+                Manage your customers' activities in the customer menu.
+              </Text>
+            </View>
+            <View style={styles.section}>
+              <View style={styles.dot} />
+              <Text style={[styles.text, { marginTop: -4 }]}>
+                Process your sales using the sales and the invoice menu
+              </Text>
+            </View>
+            <View style={styles.section}>
+              <View style={styles.dot} />
+              <Text style={[styles.text, { marginTop: -4 }]}>
+                Expense and banking menus help with your finance
+              </Text>
+            </View>
+          </Content>
         </View>
       </React.Fragment>
     )
@@ -70,6 +110,26 @@ const styles = StyleSheet.create({
     fontSize: 30,
     padding: 20,
     textAlign: 'center',
-    fontFamily: 'SourceSansPro-Semibold'
+    fontFamily: 'AvenirNext-Bold'
+  },
+  text: {
+    fontFamily: 'AvenirNext-Regular',
+    fontSize: 17,
+    textAlign: 'justify',
+    marginRight: 30,
+    color: color.textColor
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginRight: 15,
+    backgroundColor: color.textColor
+  },
+  section: {
+    flexDirection: 'row',
+    marginBottom: 30,
+    marginRight: 30,
+    alignItems: 'flex-start'
   }
 })
