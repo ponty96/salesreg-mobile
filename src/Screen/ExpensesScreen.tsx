9478b8f878e4b67a5cc06c7575d2a10bcf63eb03
klaz <giwaklaz@gmail.com>
2018-12-28 11:29:20 +0100

    changed the occurences of lets to let's

--- src/Screen/ExpensesScreen.tsx
+++ src/Screen/ExpensesScreen.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react'
-import { Alert } from 'react-native'
-import Header from '../Components/Header/BaseHeader'
-import GenericListIndex from '../Components/Generic/ListIndex'
-import { ListCompanyExpensesGQL } from '../graphql/queries/expense'
+import * as React from "react";
+import { Alert } from "react-native";
+import Header from "../Components/Header/BaseHeader";
+import GenericListIndex from "../Components/Generic/ListIndex";
+import { ListCompanyExpensesGQL } from "../graphql/queries/expense";
 
 interface IProps {
-  navigation: any
+  navigation: any;
 }
 
 export default class ExpensesScreen extends React.Component<IProps> {
@@ -14,23 +14,23 @@ export default class ExpensesScreen extends React.Component<IProps> {
       header: (
         <Header
           title="Expenses"
-          onPressRightIcon={() => Alert.alert('Search button pressed.')}
-          onPressLeftIcon={() => navigation.navigate('DrawerToggle')}
+          onPressRightIcon={() => Alert.alert("Search button pressed.")}
+          onPressLeftIcon={() => navigation.navigate("DrawerToggle")}
         />
       )
-    }
-  }
+    };
+  };
 
   parseData = (item: any) => {
     return [
       {
         firstTopText: item.title,
-        bottomLeftFirstText: '', //item.paidTo
-        bottomLeftSecondText: '', //item.date
+        bottomLeftFirstText: "", // item.paidTo
+        bottomLeftSecondText: "", // item.date
         topRightText: `\u20A6 ${item.totalAmount}`
       }
-    ]
-  }
+    ];
+  };
 
   render() {
     return (
@@ -40,14 +40,14 @@ export default class ExpensesScreen extends React.Component<IProps> {
         graphqlQueryResultKey="companyExpenses"
         parseItemData={this.parseData}
         onItemPress={item =>
-          this.props.navigation.navigate('ExpensesDetails', { expense: item })
+          this.props.navigation.navigate("ExpensesDetails", { expense: item })
         }
-        emptyListText={`Your business grows richer when your \nexpenses are under control. No better \nway to control your expenses than keeping a detailed record of your \nspendings \n\nLets proceed by tapping the`}
+        emptyListText={`Your business grows richer when your \nexpenses are under control. No better \nway to control your expenses than keeping a detailed record of your \nspendings \n\nLet's proceed by tapping the`}
         headerText="Great habit keeping records!"
         fabRouteName="UpsertExpense"
         fabIconName="database-minus"
         fabIconType="MaterialCommunityIcons"
       />
-    )
+    );
   }
 }
--- src/Screen/UpsertSalesOrderScreen.tsx
+++ src/Screen/UpsertSalesOrderScreen.tsx
@@ -1,44 +1,44 @@
-import React from 'react'
-import FormStepperContainer from '../Container/Form/StepperContainer'
-import { Mutation } from 'react-apollo'
-import AppSpinner from '../Components/Spinner'
-import RNPaystack from 'react-native-paystack'
-import Auth from '../services/auth'
-import { UpsertSaleOrder } from '../graphql/mutations/order'
-import { ListCompanySalesGQL } from '../graphql/queries/order'
-import { CompanyCustomersGQL } from '../graphql/queries/contact'
-import { parseFieldErrors } from '../Functions'
+import React from "react";
+import FormStepperContainer from "../Container/Form/StepperContainer";
+import { Mutation } from "react-apollo";
+import AppSpinner from "../Components/Spinner";
+import RNPaystack from "react-native-paystack";
+import Auth from "../services/auth";
+import { UpsertSaleOrder } from "../graphql/mutations/order";
+import { ListCompanySalesGQL } from "../graphql/queries/order";
+import { CompanyCustomersGQL } from "../graphql/queries/contact";
+import { parseFieldErrors } from "../Functions";
 
 interface IProps {
-  navigation: any
+  navigation: any;
 }
 
 interface ISalesInput {
-  productId?: string
-  quantity: String
-  name?: string
-  type?: string
-  serviceId?: String
-  unitPrice: String
+  productId?: string;
+  quantity: String;
+  name?: string;
+  type?: string;
+  serviceId?: String;
+  unitPrice: String;
 }
 
 interface IState {
-  items: ISalesInput[]
-  fieldErrors: any
-  paymentMethod: string
-  date: string
-  loading: boolean
-  existingContact: { id?: string; contactName?: string; email?: string }
-  contactName: string
-  email: string
-  isCustomerInContacts: any
-  discount: string
-  amountPaid: string
-  salesOrderId: string
-  hasSalesOrderBeenCreated: boolean
-  tax: string
-  cardDetails: any
-  user: { userId?: string; companyId?: string }
+  items: ISalesInput[];
+  fieldErrors: any;
+  paymentMethod: string;
+  date: string;
+  loading: boolean;
+  existingContact: { id?: string; contactName?: string; email?: string };
+  contactName: string;
+  email: string;
+  isCustomerInContacts: any;
+  discount: string;
+  amountPaid: string;
+  salesOrderId: string;
+  hasSalesOrderBeenCreated: boolean;
+  tax: string;
+  cardDetails: any;
+  user: { userId?: string; companyId?: string };
 }
 
 export default class UpsertSalesOrderScreen extends React.PureComponent<
@@ -47,81 +47,81 @@ export default class UpsertSalesOrderScreen extends React.PureComponent<
 > {
   static navigationOptions = {
     header: null
-  }
+  };
 
   state = {
     items: [
       {
         productId: null,
-        quantity: '',
-        name: '',
-        type: '',
+        quantity: "",
+        name: "",
+        type: "",
         serviceId: null,
-        unitPrice: '0.00'
+        unitPrice: "0.00"
       }
     ],
     fieldErrors: null,
     isCustomerInContacts: null,
-    paymentMethod: '',
-    amountPaid: '0.00',
+    paymentMethod: "",
+    amountPaid: "0.00",
     date: new Date().toString(),
-    discount: '0',
-    existingContact: { id: '', contactName: '', email: '' },
-    tax: '',
-    contactName: '',
-    email: '',
+    discount: "0",
+    existingContact: { id: "", contactName: "", email: "" },
+    tax: "",
+    contactName: "",
+    email: "",
     loading: false,
-    salesOrderId: '',
+    salesOrderId: "",
     hasSalesOrderBeenCreated: false,
     cardDetails: null,
-    user: { companyId: '', userId: '' }
-  }
+    user: { companyId: "", userId: "" }
+  };
 
   async componentDidMount() {
-    let {
+    const {
       company: { id: companyId },
       id: userId
-    } = JSON.parse(await Auth.getCurrentUser())
+    } = JSON.parse(await Auth.getCurrentUser());
     this.setState({
       user: {
         companyId,
         userId
       }
-    })
+    });
   }
 
   updateState = (key: string, val: any) => {
     const formData = {
       ...this.state,
       [key]: val
-    }
+    };
 
-    let total = 0
+    let total = 0;
 
-    if (key == 'items') {
+    if (key == "items") {
       val.forEach(item => {
-        total += Number(item.unitPrice) * Number(item.quantity)
-      })
+        total += Number(item.unitPrice) * Number(item.quantity);
+      });
     }
 
     this.setState({
       ...formData,
       amountPaid:
-        key == 'items'
+        key == "items"
           ? total.toString()
-          : key == 'amountPaid'
+          : key == "amountPaid"
           ? val
           : this.state.amountPaid
-    })
-  }
+    });
+  };
 
   onCompleted = async res => {
     const {
       upsertSaleOrder: { success, fieldErrors, data }
-    } = res
+    } = res;
 
     if (success) {
-      let { id } = data
+      const { id } = data;
 
       this.setState(
         {
@@ -129,76 +129,76 @@ export default class UpsertSalesOrderScreen extends React.PureComponent<
           hasSalesOrderBeenCreated: true
         },
         () => {
-          this.state.paymentMethod.toLowerCase() == 'cash'
-            ? this.props.navigation.navigate('Sales')
-            : this.chargeCard()
+          this.state.paymentMethod.toLowerCase() == "cash"
+            ? this.props.navigation.navigate("Sales")
+            : this.chargeCard();
         }
-      )
+      );
     } else {
-      this.setState({ fieldErrors: parseFieldErrors(fieldErrors) })
+      this.setState({ fieldErrors: parseFieldErrors(fieldErrors) });
     }
-  }
+  };
 
   chargeCard = async () => {
-    let { cardDetails, amountPaid } = this.state,
+    const { cardDetails, amountPaid } = this.state,
       _cardDetails = cardDetails || {},
-      { valid } = _cardDetails
+      { valid } = _cardDetails;
 
     if (valid) {
-      let {
+      const {
         values: { number, expiry, cvc }
-      } = _cardDetails
+      } = _cardDetails;
 
-      this.setState({ loading: true })
+      this.setState({ loading: true });
       RNPaystack.chargeCard({
-        cardNumber: number.replace(/\s/gi, ''),
-        expiryMonth: expiry.split('/')[0],
-        expiryYear: expiry.split('/')[1],
+        cardNumber: number.replace(/\s/gi, ""),
+        expiryMonth: expiry.split("/")[0],
+        expiryYear: expiry.split("/")[1],
         cvc,
         email:
-          this.state.isCustomerInContacts != 'No'
+          this.state.isCustomerInContacts != "No"
             ? this.state.existingContact.email
             : this.state.email,
         amountInKobo: Number(amountPaid) * 100,
         reference: `${this.state.salesOrderId}_${Date.now()}`
       })
         .then(() => {
-          this.setState({ loading: false })
-          this.props.navigation.navigate('Sales')
+          this.setState({ loading: false });
+          this.props.navigation.navigate("Sales");
         })
         .catch(error => {
-          //@Todo: handle error better
-          console.log(error.message)
-          this.setState({ loading: false })
-        })
+          // @Todo: handle error better
+          console.log(error.message);
+          this.setState({ loading: false });
+        });
     } else {
-      console.log('Card details entered is invalid')
+      console.log("Card details entered is invalid");
     }
-  }
+  };
 
   parseMutationVariables = () => {
-    let contact = {}
+    let contact = {};
 
-    if (this.state.isCustomerInContacts == 'No') {
+    if (this.state.isCustomerInContacts == "No") {
       contact = {
         ...this.state.user,
         contactName: this.state.contactName,
         email: this.state.email,
-        type: 'customer'
-      }
+        type: "customer"
+      };
     }
 
-    let items = this.state.items.map(item => {
-      let param = { ...item }
-      delete param.type
-      delete param.name
+    const items = this.state.items.map(item => {
+      const param = { ...item };
+      delete param.type;
+      delete param.name;
 
-      if (!param.productId) delete param.productId
-      if (!param.serviceId) delete param.serviceId
-      return param
-    })
+      if (!param.productId) delete param.productId;
+      if (!param.serviceId) delete param.serviceId;
+      return param;
+    });
 
-    let {
+    const {
         navigation: {
           state: { params }
         }
@@ -211,38 +211,38 @@ export default class UpsertSalesOrderScreen extends React.PureComponent<
         items,
         paymentMethod: this.state.paymentMethod.toUpperCase(),
         ...this.state.user
-      }
+      };
 
-    if (_params.isCustomerInContacts != 'No') {
-      _params['contactId'] = _params.existingContact.id
-      delete _params.contact
+    if (_params.isCustomerInContacts != "No") {
+      _params["contactId"] = _params.existingContact.id;
+      delete _params.contact;
     }
 
-    delete _params.cardDetails
-    delete _params.fieldErrors
-    delete _params.existingContact
-    delete _params.isCustomerInContacts
-    delete _params.user
-    delete _params.salesOrderId
-    delete _params.hasSalesOrderBeenCreated
-    delete _params.loading
-    delete _params.email
-    delete _params.contactName
+    delete _params.cardDetails;
+    delete _params.fieldErrors;
+    delete _params.existingContact;
+    delete _params.isCustomerInContacts;
+    delete _params.user;
+    delete _params.salesOrderId;
+    delete _params.hasSalesOrderBeenCreated;
+    delete _params.loading;
+    delete _params.email;
+    delete _params.contactName;
 
     return saleId
       ? {
           sale: _params,
           saleId
         }
-      : { sale: _params }
-  }
+      : { sale: _params };
+  };
 
   render() {
-    let {
+    const {
       isCustomerInContacts,
       user: { companyId },
       hasSalesOrderBeenCreated
-    } = this.state
+    } = this.state;
 
     return (
       <Mutation
@@ -251,7 +251,7 @@ export default class UpsertSalesOrderScreen extends React.PureComponent<
           {
             query: ListCompanySalesGQL,
             variables: {
-              companyId: companyId,
+              companyId,
               first: 10,
               after: null
             }
@@ -277,138 +277,138 @@ export default class UpsertSalesOrderScreen extends React.PureComponent<
               }
               steps={[
                 {
-                  stepTitle: 'Lets have the items that are being ordered',
+                  stepTitle: "Let's have the items that are being ordered",
                   formFields: [
                     {
-                      label: '',
-                      validators: ['sales-order'],
+                      label: "",
+                      validators: ["sales-order"],
                       type: {
-                        type: 'sales-order-items'
+                        type: "sales-order-items"
                       },
-                      name: 'items'
+                      name: "items"
                     }
                   ]
                 },
                 {
-                  stepTitle: 'Who is making this order?',
+                  stepTitle: "Who is making this order?",
                   formFields: [
                     {
-                      label: 'Is this customer in your contacts?',
-                      validators: ['required'],
+                      label: "Is this customer in your contacts?",
+                      validators: ["required"],
                       type: {
-                        type: 'radio',
-                        options: ['No', 'Yes']
+                        type: "radio",
+                        options: ["No", "Yes"]
                       },
-                      name: 'isCustomerInContacts'
+                      name: "isCustomerInContacts"
                     },
-                    isCustomerInContacts == 'No' && {
-                      label: 'Customer name?',
-                      validators: ['required'],
+                    isCustomerInContacts == "No" && {
+                      label: "Customer name?",
+                      validators: ["required"],
                       type: {
-                        type: 'input'
+                        type: "input"
                       },
                       placeholder: "Enter customer's name",
-                      name: 'contactName',
+                      name: "contactName",
                       underneathText:
-                        'This customer will be added to your contacts. You can edit this contact through details.'
+                        "This customer will be added to your contacts. You can edit this contact through details."
                     },
-                    isCustomerInContacts == 'No'
+                    isCustomerInContacts == "No"
                       ? {
-                          label: 'Customer Email?',
-                          validators: ['required', 'email'],
+                          label: "Customer Email?",
+                          validators: ["required", "email"],
                           type: {
-                            type: 'input',
-                            keyboardType: 'email-address'
+                            type: "input",
+                            keyboardType: "email-address"
                           },
                           placeholder: "Enter customer's email",
-                          name: 'email'
+                          name: "email"
                         }
-                      : isCustomerInContacts == 'Yes'
+                      : isCustomerInContacts == "Yes"
                       ? {
-                          label: 'Customer',
-                          validators: ['required'],
+                          label: "Customer",
+                          validators: ["required"],
                           type: {
-                            type: 'search-picker',
+                            type: "search-picker",
                             searchQuery: CompanyCustomersGQL,
                             emptySection: {
                               emptyText:
-                                'You currently do not have any customers. \n To create a new customer, open the side navigation drawer > customers. Afterwards, you can come back here to create your sales order'
+                                "You currently do not have any customers. \n To create a new customer, open the side navigation drawer > customers. Afterwards, you can come back here to create your sales order"
                             },
-                            searchQueryResponseKey: 'companyCustomers'
+                            searchQueryResponseKey: "companyCustomers"
                           },
-                          name: 'existingContact',
-                          placeholder: 'Touch to select customer'
+                          name: "existingContact",
+                          placeholder: "Touch to select customer"
                         }
                       : null
                   ]
                 },
                 {
-                  stepTitle: 'Payment Method',
+                  stepTitle: "Payment Method",
                   formFields: [
                     {
-                      label: 'How is this customer paying?',
-                      validators: ['required'],
+                      label: "How is this customer paying?",
+                      validators: ["required"],
                       type: {
-                        type: 'radio',
-                        options: ['Card', 'Cash']
+                        type: "radio",
+                        options: ["Card", "Cash"]
                       },
-                      name: 'paymentMethod'
+                      name: "paymentMethod"
                     },
                     {
-                      label: 'How much(N) was actually paid?',
-                      validators: ['required'],
+                      label: "How much(N) was actually paid?",
+                      validators: ["required"],
                       type: {
-                        type: 'input',
-                        keyboardType: 'numeric'
+                        type: "input",
+                        keyboardType: "numeric"
                       },
-                      placeholder: '0.00',
-                      name: 'amountPaid'
+                      placeholder: "0.00",
+                      name: "amountPaid"
                     },
                     {
-                      label: 'Are you giving discounts?',
+                      label: "Are you giving discounts?",
                       type: {
-                        type: 'input',
-                        keyboardType: 'numeric'
+                        type: "input",
+                        keyboardType: "numeric"
                       },
-                      placeholder: '0',
-                      name: 'discount',
-                      underneathText: 'Ignore if there are no discounts.'
+                      placeholder: "0",
+                      name: "discount",
+                      underneathText: "Ignore if there are no discounts."
                     },
                     {
-                      label: 'Tax on this payment(%)',
+                      label: "Tax on this payment(%)",
                       type: {
-                        type: 'input',
-                        keyboardType: 'numeric'
+                        type: "input",
+                        keyboardType: "numeric"
                       },
-                      placeholder: '0.00',
-                      name: 'tax',
+                      placeholder: "0.00",
+                      name: "tax",
                       underneathText:
-                        'Ignore if there are no taxes on this payment'
+                        "Ignore if there are no taxes on this payment"
                     }
                   ],
                   buttonTitle:
-                    this.state.paymentMethod.toLowerCase() != 'cash'
-                      ? 'Next'
-                      : 'Done'
+                    this.state.paymentMethod.toLowerCase() != "cash"
+                      ? "Next"
+                      : "Done"
                 },
-                this.state.paymentMethod.toLowerCase() == 'card' && {
-                  stepTitle: 'Lets sort out the payment for this order',
+                this.state.paymentMethod.toLowerCase() == "card" && {
+                  stepTitle: "Let's sort out the payment for this order",
                   formFields: [
                     {
-                      label: '',
+                      label: "",
                       type: {
-                        type: 'card-payment'
+                        type: "card-payment"
                       },
-                      name: 'cardDetails'
+                      name: "cardDetails"
                     }
                   ],
-                  buttonTitle: 'Done'
+                  buttonTitle: "Done"
                 }
               ]}
             />
           </React.Fragment>
         )}
       </Mutation>
-    )
+    );
   }
 }
