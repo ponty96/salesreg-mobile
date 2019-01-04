247dba6a98497b6d7d6bae40ac8edb62f772ae00
klaz <giwaklaz@gmail.com>
2019-01-04 21:16:17 +0100

    added images to landing page

--- src/Screen/Onboarding/Landing.tsx
+++ src/Screen/Onboarding/Landing.tsx
@@ -1,55 +1,68 @@
-import React, { PureComponent } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { PureComponent } from "react";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
 
-import ButtonAtom from '../../Atom/Form/ButtonAtom'
-import { color } from '../../Style/Color';
+import ButtonAtom from "../../Atom/Form/ButtonAtom";
+import { color } from "../../Style/Color";
 // import OnboardingContainer from '../../Container/OnboardingContainer'
 
 interface IProps {
-  navigation: any
+  navigation: any;
 }
 
 class LandingScreen extends PureComponent<IProps> {
   render() {
-    const { navigate } = this.props.navigation
+    const { navigate } = this.props.navigation;
     return (
-    //   <OnboardingContainer>
-        <View style={styles.container}>
-          <ButtonAtom
-            btnText="Get Started"
-            onPress={() => navigate('Signup')}
-            type="primary"
+      <View style={styles.container}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+        >
+          <Image
+            source={require("../../../assets-v1/images/onboardingScreen/ecommerce.png")}
           />
-          <Text style={styles.haveAccount}>Already have an account?</Text>
+          <Image
+            source={require("../../../assets-v1/images/onboardingScreen/delivery.png")}
+          />
+          <Image
+            source={require("../../../assets-v1/images/onboardingScreen/restock.png")}
+          />
+        </ScrollView>
+        <ButtonAtom
+          btnText="Get Started"
+          onPress={() => navigate("Signup")}
+          type="primary"
+        />
+        <Text style={styles.haveAccount}>Already have an account?</Text>
 
-          <ButtonAtom
-            btnText="LOGIN"
-            transparent={true}
-            onPress={() => navigate('Login')}
-            type="secondary"
-            hideIcon={true}
-          />
-        </View>
-    //   </OnboardingContainer>
-    )
+        <ButtonAtom
+          btnText="LOGIN"
+          transparent={true}
+          onPress={() => navigate("Login")}
+          type="secondary"
+          hideIcon={true}
+        />
+      </View>
+    );
   }
 }
 
-export default LandingScreen
+export default LandingScreen;
 
 const styles = StyleSheet.create({
   haveAccount: {
     marginTop: 32,
-    textAlign: 'center',
-    color: '#fff',
+    textAlign: "center",
+    color: "#fff",
     fontSize: 14,
-    fontFamily: 'AvenirNext-DemiBold'
+    fontFamily: "AvenirNext-DemiBold"
   },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+    alignItems: "center",
+    justifyContent: "flex-end",
     paddingBottom: 24,
     backgroundColor: color.button
   }
-})
+});
