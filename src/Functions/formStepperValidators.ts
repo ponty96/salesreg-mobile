f6ec3634c80e0fc4165f3532434bffb62befc222
Anifowoshe Gbenga <daveanifowoshe@ymail.com>
2019-02-11 16:01:58 +0100

    Fix issues with the formStepper validator overriding each other

--- src/Functions/formStepperValidators.ts
+++ src/Functions/formStepperValidators.ts
@@ -39,50 +39,92 @@ export const validateField = (
 
     if (validator == 'required') {
       let required = isRequired(_value)
-      isValid = required.fieldValid
-      error = required.errorMessage
+      isValid = required.fieldValid == false ? required.fieldValid : isValid
+      error = required.fieldValid == false ? required.errorMessage : error
     }
 
     if (validator == 'social-media-username') {
       let usernameValidation = validateSocialMediaUsername(_value || '')
-      isValid = usernameValidation.fieldValid
-      error = usernameValidation.errorMessage
+      isValid =
+        usernameValidation.fieldValid == false
+          ? usernameValidation.fieldValid
+          : isValid
+      error =
+        usernameValidation.fieldValid == false
+          ? usernameValidation.errorMessage
+          : error
     }
 
     if (validator == 'email') {
       let emailValidation = validateEmail(_value)
-      isValid = emailValidation.fieldValid
-      error = emailValidation.errorMessage
+      isValid =
+        emailValidation.fieldValid == false
+          ? emailValidation.fieldValid
+          : isValid
+      error =
+        emailValidation.fieldValid == false
+          ? emailValidation.errorMessage
+          : error
     }
 
     if (validator == 'phone') {
       let phoneValidation = validatePhone(_value)
-      isValid = phoneValidation.fieldValid
-      error = phoneValidation.errorMessage
+      isValid =
+        phoneValidation.fieldValid == false
+          ? phoneValidation.fieldValid
+          : isValid
+      error =
+        phoneValidation.fieldValid == false
+          ? phoneValidation.errorMessage
+          : error
     }
 
     if (validator == 'credit-card') {
       let creditCardValidation = validateCreditCard(_value)
-      isValid = creditCardValidation.fieldValid
-      error = creditCardValidation.errorMessage
+      isValid =
+        creditCardValidation.fieldValid == false
+          ? creditCardValidation.fieldValid
+          : isValid
+      error =
+        creditCardValidation.fieldValid == false
+          ? creditCardValidation.errorMessage
+          : error
     }
 
     if (validator == 'sales-order') {
       let salesOrderValiation = validateSalesOrder(_value)
-      isValid = salesOrderValiation.fieldValid
-      error = salesOrderValiation.errorMessage
+      isValid =
+        salesOrderValiation.fieldValid == false
+          ? salesOrderValiation.fieldValid
+          : isValid
+      error =
+        salesOrderValiation.fieldValid == false
+          ? salesOrderValiation.errorMessage
+          : error
     }
 
     if (validator == 'expense-item') {
       let expenseValiation = validateExpenseItem(_value)
-      isValid = expenseValiation.fieldValid
-      error = expenseValiation.errorMessage
+      isValid =
+        expenseValiation.fieldValid == false
+          ? expenseValiation.fieldValid
+          : isValid
+      error =
+        expenseValiation.fieldValid == false
+          ? expenseValiation.errorMessage
+          : error
     }
 
     if (validator == 'password') {
       let passwordValiation = validatePassword(_value)
-      isValid = passwordValiation.fieldValid
-      error = passwordValiation.errorMessage
+      isValid =
+        passwordValiation.fieldValid == false
+          ? passwordValiation.fieldValid
+          : isValid
+      error =
+        passwordValiation.fieldValid == false
+          ? passwordValiation.errorMessage
+          : error
     }
 
     if (validator == 'confirm-password') {
@@ -90,8 +132,14 @@ export const validateField = (
         _value,
         passwordFieldValue
       )
-      isValid = passwordConfirmValidation.fieldValid
-      error = passwordConfirmValidation.errorMessage
+      isValid =
+        passwordConfirmValidation.fieldValid == false
+          ? passwordConfirmValidation.fieldValid
+          : isValid
+      error =
+        passwordConfirmValidation.fieldValid == false
+          ? passwordConfirmValidation.errorMessage
+          : error
     }
   })
 
